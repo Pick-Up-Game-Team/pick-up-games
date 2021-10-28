5adcd9891fb352c729a0c37be446a6fd9bb0cd6a
William Readyhough <william.readyhough@gmail.com>
2021-10-28 19:42:25 -0400

    Added our home page! It's very empty atm tho

--- pick_up_games/settings.py
+++ pick_up_games/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registrationPage.apps.RegistrationpageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
