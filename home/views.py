605d43eb47a1d845cf9139ff07daaac9441eefb8
William Readyhough <william.readyhough@gmail.com>
2021-10-28 22:57:37 -0400

    Commit before I attempt to do migrations/database stuff.

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Home page currently in progress. This is here temporarily :)")
+    return HttpResponse("<h1>Home page currently in progress. This is here temporarily :)<h1>")
