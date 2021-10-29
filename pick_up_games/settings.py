af66a4042a8f6dae9106f76c6d0232c2a5080ed3
William Readyhough <william.readyhough@gmail.com>
2021-10-29 09:01:49 -0400

    Added user app to store registration info

--- pick_up_games/settings.py
+++ pick_up_games/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'registrationPage.apps.RegistrationpageConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
