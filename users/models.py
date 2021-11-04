b23a4e5faeb4df1ae650dd8be2693e6da1aafa93
kennetk1 <KenJesus07>
2021-11-03 20:46:20 -0400

    Creating the Profile Model, Giving it new fields to fill out, and registering it

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default ='default.jpg', upload_to='profile_pics')
+    height = models.CharField(max_length = 10)
+    #Date of Birth
+    dob = models.DateTimeField()
+    #ToDO (Kenneth)  Tempoary PlaceHolder for the sports colum
+    sports = models.TextField(default = 'No Sports Played')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
