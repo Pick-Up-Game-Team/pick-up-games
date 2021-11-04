8ae386c24f76138ec6c657fcf2df6b2b9d68c368
kennetk1 <KenJesus07>
2021-11-03 22:05:48 -0400

    Added the signals and Migrations Added the functionality that the profile will be created with the user

--- users/models.py
+++ users/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default ='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default ='default.png', upload_to='profile_pics')
     height = models.CharField(max_length = 10)
     #Date of Birth
-    dob = models.DateTimeField()
+    dob = models.DateTimeField(default=timezone.now)
     #ToDO (Kenneth)  Tempoary PlaceHolder for the sports colum
     sports = models.TextField(default = 'No Sports Played')
 
