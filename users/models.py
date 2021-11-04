75bc80213aa4a65b8ad3978ff3a39618bd3aa616
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-04 01:43:26 -0400

    Fully Implemented Profile Update w/IconResize

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
+from PIL import Image
 
 # Create your models here.
 
@@ -16,3 +17,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        # save image for editing
+        super().save()
+        profileIMG = Image.open(self.image.path)
+
+        # resizing image
+        if profileIMG.height >  300 or profileIMG.width > 300:
+            output_size = (300, 300)
+            profileIMG.thumbnail(output_size)
+            profileIMG.save(self.image.path)
