3ba5487b51e4c40f9d38f446c8e8aa89e39deb2b
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-03 22:42:16 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 
 # Create your models here.
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default ='default.png', upload_to='profile_pics')
