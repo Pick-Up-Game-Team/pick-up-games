e3b1c8346a1dc20d36686558dee9dd854de89b92
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-03 20:09:18 -0400

    Preliminary Additions for

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm
 
 def register(request):
     #This one will create the actual user creation form
