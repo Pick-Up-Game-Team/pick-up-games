f99ce028b27837b8058dee03d4b9f7b08f7aadce
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-04 20:24:09 -0400

    Revert "Update models.py"
    This reverts commit 3ba5487b51e4c40f9d38f446c8e8aa89e39deb2b.

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default ='default.png', upload_to='profile_pics')
