39298108ddb0563f965601ab7607e2a224aaeed5
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-04 20:24:15 -0400

    Revert "Merge branch 'Feature_User_Profile' into UpdateProfile"
    This reverts commit 64dc564ef87409a46ce99f290de73c2c008bfa92, reversing
    changes made to d7e0b53a8137b8ec3962c871d9c9275b29c476d5.

--- users/models.py
+++ users/models.py
@@ -1,18 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
-# Create your models here.
-
-
+# User model
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default ='default.png', upload_to='profile_pics')
-    height = models.CharField(max_length = 10)
-    #Date of Birth
-    dob = models.DateTimeField(default=timezone.now)
-    #ToDO (Kenneth)  Tempoary PlaceHolder for the sports colum
-    sports = models.TextField(default = 'No Sports Played')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+    # Profile fields below
--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm
 
 def register(request):
