081ece66db7efc3de7968bdfaf6ca5e0341ec755
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-04 20:24:02 -0400

    Revert "wrapping up profile creation and update forms"
    This reverts commit 773ddff3a55d8fb42ddbc4d17fa53fcc5d20bd55.

--- users/forms.py
+++ users/forms.py
@@ -22,4 +22,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        field = ['image']
--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 
 # Create your models here.
