09098e4ad7e82cb8111f8084f18e24437dcdd818
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-04 20:23:57 -0400

    Revert "small changes"
    This reverts commit 38a3b29f45d65363544774d847e7aac5245125d1.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import (UserRegisterForm, UserUpdateForm,
+ProfileUpdateForm)
 
 def register(request):
     #This one will create the actual user creation form
