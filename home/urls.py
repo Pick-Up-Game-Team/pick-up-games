a67b1a3cee829bc23fb273b01aec8ed27a10fcfe
Thanh Nam Tran <ttran19@umbc.edu>
2021-11-09 22:27:58 -0500

    homepage enhancement

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home-page'),
-
+    path('support/', views.support, name='support-page'),
+    path('about/', views.about, name='about-page'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home/home-page.html')
+
+
+def support(request):
+    return render(request, 'home/support.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
