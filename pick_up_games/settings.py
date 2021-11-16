15e13e6d5e4b57c459de7a01652d16af541c9477
dvuong1 <78446094+dvuong1@users.noreply.github.com>
2021-11-16 18:43:54 -0500

    Get secret key from env variable instead of config file

--- pick_up_games/settings.py
+++ pick_up_games/settings.py
@@ -11,10 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from . import config
 import os
 import django_heroku
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+# SECRET_KEY should be set as an environment variable for deployment
+SECRET_KEY = os.getenv('SECRET_KEY', 'local_supersecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
