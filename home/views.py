ea5d239f743d6f0a254f51ce0bf7dc5c0a26386a
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-17 11:52:22 -0500

    set up search bar and search results page
    can take in user input to display a page that will eventually show search results

--- home/views.py
+++ home/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home-page.html')
+
+def search(request):
+    if request.method == "POST":
+        results = request.POST['results']
+        return render(request, 'home/search.html', {'results':results})
+    else:
+        return render(request, 'home/search.html')
--- pick_up_games/urls.py
+++ pick_up_games/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    #path(register/'', include('users.urls')),
     path('register/', user_views.register, name='registration'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
