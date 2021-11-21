42a14f650f8370df4acae0a3aa5d563527fd76af
trackstarc <cedrick.tanon.2018@gmail.com>
2021-11-20 21:58:43 -0500

    small changes

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'home/home-page.html')
@@ -7,6 +8,7 @@ def home(request):
 def search(request):
     if request.method == "POST":
         results = request.POST['results']
-        return render(request, 'home/search.html', {'results':results})
+        queries = User.objects.filter(username__contains=results)
+        return render(request, 'home/search.html', {'results':results, 'queries':queries})
     else:
         return render(request, 'home/search.html')
